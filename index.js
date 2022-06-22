ef3d0b13347c2243bfd3b3eff6c819991cab1369
Ushie <ushiekane@gmail.com>
2022-06-22 21:43:49 +0300

    oops

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ document.getElementById("submitButton").onclick = function(){
     if(document.getElementById("cButton").checked){
         temp = document.getElementById("textBox").value;
         temp = Number(temp);
-        temp = temp * 9/5 + 32;
+        temp = (temp - 32) * (5/9);
         document.getElementById("tempLabel").innerHTML = temp + "°C";
     }
     else if(document.getElementById("fButton").checked){
         temp = document.getElementById("textBox").value;
         temp = Number(temp);
-        temp = (temp - 32) * (5/9);
+        temp = temp * 9/5 + 32;
         document.getElementById("tempLabel").innerHTML = temp + "°F";
     }
     else{
