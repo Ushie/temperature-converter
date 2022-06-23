cb26a846f1b797c7fbbeb0fd55e85b0697d5f6b4
Ushie <ushiekane@gmail.com>
2022-06-23 20:04:24 +0300

    a bunch of spaghetti code (added kelvin)

--- index.js
+++ index.js
@@ -2,30 +2,72 @@ document.getElementById("submitButton").onclick = function(){
 
     let temp;
 
-    if(document.getElementById("cButton").checked){
-        temp = document.getElementById("textBox").value;
-        temp = Number(temp);
-        temp = toCelsius(temp);
-        document.getElementById("tempLabel").innerHTML = temp + "°C";
+    if(document.getElementById("fromCelsius").checked){
+        if(document.getElementById("toCelsius").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°C";
+        }
+        else if(document.getElementById("toFahrenheit").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            temp = temp * 9/5 + 32;
+            Math.round(temp);
+            document.getElementById("tempLabel").innerHTML = temp + "°F";
+        }
+        else if(document.getElementById("toKelvin").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            temp = temp + 273.15;
+            document.getElementById("tempLabel").innerHTML = temp + "°K";
+        }
+        else{
+            document.getElementById("tempLabel").innerHTML = "you haven't chosen what you want to convert to!"
+        }
     }
-    else if(document.getElementById("fButton").checked){
-        temp = document.getElementById("textBox").value;
-        temp = Number(temp);
-        temp = toFahrenheit(temp);
-        document.getElementById("tempLabel").innerHTML = temp + "°F";
+    else if(document.getElementById("fromFahrenheit").checked){
+        if(document.getElementById("toCelsius").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            temp = (temp - 32) * (5/9);
+            Math.round(temp);
+            document.getElementById("tempLabel").innerHTML = temp + "°C";
+        }
+        else if(document.getElementById("toFahrenheit").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°F";
+        }
+        else if(document.getElementById("toKelvin").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            temp = (temp - 32) * 5/9 + 273.15;
+            document.getElementById("tempLabel").innerHTML = temp + "°K";
+        }
+    }
+    else if(document.getElementById("fromKelvin").checked){
+        if(document.getElementById("toCelsius").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            temp = temp - 273.15;
+            document.getElementById("tempLabel").innerHTML = temp + "°C"
+        }
+        else if(document.getElementById("toFahrenheit").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            temp = (temp - 273.15) * 9/5 + 32;
+            document.getElementById("tempLabel").innerHTML = temp + "°C"
+        }
+        else if(document.getElementById("toKelvin").checked){
+            temp = document.getElementById("textBox").value;
+            temp = Number(temp);
+            document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°K";
+        }
+        else{
+            document.getElementById("tempLabel").innerHTML = "you haven't chosen what you want to convert to!"
+        }
     }
     else{
         document.getElementById("tempLabel").innerHTML = "You haven't selected a unit!";
-
     }
-}
-
-function toCelsius(temp){
-    temp = (temp - 32) * (5/9);
-    return Math.round(temp);
-}
-
-function toFahrenheit(temp){
-    temp = temp * 9/5 + 32;
-    return Math.round(temp);
 }
