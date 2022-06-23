8a54e66c0f3ded7571787ff2d9876c337db6f3ff
Ushie <ushiekane@gmail.com>
2022-06-23 19:26:01 +0300

    added: rounding the total

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ document.getElementById("submitButton").onclick = function(){
     if(document.getElementById("cButton").checked){
         temp = document.getElementById("textBox").value;
         temp = Number(temp);
-        temp = (temp - 32) * (5/9);
+        temp = toCelsius(temp);
         document.getElementById("tempLabel").innerHTML = temp + "°C";
     }
     else if(document.getElementById("fButton").checked){
         temp = document.getElementById("textBox").value;
         temp = Number(temp);
-        temp = temp * 9/5 + 32;
+        temp = toFahrenheit(temp);
         document.getElementById("tempLabel").innerHTML = temp + "°F";
     }
     else{
         document.getElementById("tempLabel").innerHTML = "You haven't selected a unit!";
 
     }
+}
+
+function toCelsius(temp){
+    temp = (temp - 32) * (5/9);
+    return Math.round(temp);
+}
+
+function toFahrenheit(temp){
+    temp = temp * 9/5 + 32;
+    return Math.round(temp);
 }
