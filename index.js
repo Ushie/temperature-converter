c21206b72585113bdc88b693193ef019558c5b08
Ushie <ushiekane@gmail.com>
2022-06-24 05:03:24 +0300

    fix up spaghetti code

--- index.js
+++ index.js
@@ -1,73 +1,91 @@
 document.getElementById("submitButton").onclick = function(){
 
-    let temp;
+    let temp = getTemp();
 
     if(document.getElementById("fromCelsius").checked){
         if(document.getElementById("toCelsius").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°C";
+            doofus(temp);
         }
         else if(document.getElementById("toFahrenheit").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            temp = temp * 9/5 + 32;
-            Math.round(temp);
-            document.getElementById("tempLabel").innerHTML = temp + "°F";
+            temp = temp + 273.15;
+            temp = toFahrenheit(temp);
+            print(temp);
         }
         else if(document.getElementById("toKelvin").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
             temp = temp + 273.15;
-            document.getElementById("tempLabel").innerHTML = temp + "°K";
+            print(temp);
         }
         else{
-            document.getElementById("tempLabel").innerHTML = "you haven't chosen what you want to convert to!"
+            noSelection()
         }
     }
     else if(document.getElementById("fromFahrenheit").checked){
         if(document.getElementById("toCelsius").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            temp = (temp - 32) * (5/9);
-            Math.round(temp);
-            document.getElementById("tempLabel").innerHTML = temp + "°C";
+            temp = (temp - 32) * 5/9 + 273.15;
+            temp = toCelsius(temp);
+            print(temp);
         }
         else if(document.getElementById("toFahrenheit").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°F";
+            doofus(temp);
         }
         else if(document.getElementById("toKelvin").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
             temp = (temp - 32) * 5/9 + 273.15;
-            document.getElementById("tempLabel").innerHTML = temp + "°K";
+            print(temp);
+        }
+        else{
+            noSelection()
         }
     }
     else if(document.getElementById("fromKelvin").checked){
         if(document.getElementById("toCelsius").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            temp = temp - 273.15;
-            document.getElementById("tempLabel").innerHTML = temp + "°C"
+            temp = toCelsius(temp);
+            print(temp);
         }
         else if(document.getElementById("toFahrenheit").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            temp = (temp - 273.15) * 9/5 + 32;
-            document.getElementById("tempLabel").innerHTML = temp + "°C"
+            temp = toFahrenheit(temp);
+            print(temp);
         }
         else if(document.getElementById("toKelvin").checked){
-            temp = document.getElementById("textBox").value;
-            temp = Number(temp);
-            document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°K";
+            doofus(temp);
         }
         else{
-            document.getElementById("tempLabel").innerHTML = "you haven't chosen what you want to convert to!"
+            noSelection()
         }
     }
-    else{
-        document.getElementById("tempLabel").innerHTML = "You haven't selected a unit!";
+}
+
+function doofus(temp){
+    if(document.getElementById("toCelsius").checked){
+        document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°C";
     }
+    else if(document.getElementById("toFahrenheit").checked){
+        document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°F";
+    }
+    else{
+        document.getElementById("tempLabel").innerHTML = "you doofus, i refuse to do that.... okay here it is anyways: " + temp + "°K";
+    }
+}
+function print(temp){
+    if(document.getElementById("toCelsius").checked){
+        document.getElementById("tempLabel").innerHTML = temp + "°C";
+    }
+    else if(document.getElementById("toFahrenheit").checked){
+        document.getElementById("tempLabel").innerHTML = temp + "°F";
+    }
+    else{
+        document.getElementById("tempLabel").innerHTML = temp + "°K";
+    }
+}
+function getTemp(temp){
+    temp = document.getElementById("textBox").value;
+    return Number(temp);
+}
+function toCelsius(temp){
+    return temp = temp - 273.15;
+}
+function toFahrenheit(temp){
+    return (temp - 273.15) * 9/5 + 32;
+}
+function noSelection(){
+    document.getElementById("tempLabel").innerHTML = "You need to choose both!"
 }
