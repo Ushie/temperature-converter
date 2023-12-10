1e72820a875ccf60258766eaa29dc11074f1297c
Ushie <ushiekane@gmail.com>
2023-12-10 18:00:36 +0300

    refactor: formatting

--- index.js
+++ index.js
@@ -15,13 +15,34 @@ document.getElementById("submitButton").onclick = function () {
 
     // we use kelvin as the base temp, this removes the need to account for possible combination of selections
     var converter = {
-        celsius: { toKelvin: temp => temp + 273.15, fromKelvin: temp => temp - 273.15 },
-        fahrenheit: { toKelvin: temp => (temp - 32) * 5 / 9 + 273.15, fromKelvin: temp => (temp - 273.15) * 9 / 5 + 32 },
-        rankine: { toKelvin: temp => temp * 5 / 9, fromKelvin: temp => temp * 1.8 },
-        romer: { toKelvin: temp => (temp - 7.5) / 0.52500 + 273.15, fromKelvin: temp => (temp - 273.15) * 21 / 40 + 7.5 },
-        reaumur: { toKelvin: temp => temp * 4 / 5 + 273.15, fromKelvin: temp => (temp - 273.15) * 0.8 },
-        newton: { toKelvin: temp => temp * 100 / 33 + 273.15, fromKelvin: temp => (temp - 273.15) * 0.33000 },
-        delisle: { toKelvin: temp => 373.15 - (temp * 2 / 3), fromKelvin: temp => (temp - 273.15) * 1.5000 - 100.00 },
+        celsius: {
+            toKelvin: temp => temp + 273.15,
+            fromKelvin: temp => temp - 273.15
+        },
+        fahrenheit: {
+            toKelvin: temp => (temp - 32) * 5 / 9 + 273.15,
+            fromKelvin: temp => (temp - 273.15) * 9 / 5 + 32
+        },
+        rankine: {
+            toKelvin: temp => temp * 5 / 9,
+            fromKelvin: temp => temp * 1.8
+        },
+        romer: {
+            toKelvin: temp => (temp - 7.5) / 0.52500 + 273.15,
+            fromKelvin: temp => (temp - 273.15) * 21 / 40 + 7.5
+        },
+        reaumur: {
+            toKelvin: temp => temp * 4 / 5 + 273.15,
+            fromKelvin: temp => (temp - 273.15) * 0.8
+        },
+        newton: {
+            toKelvin: temp => temp * 100 / 33 + 273.15,
+            fromKelvin: temp => (temp - 273.15) * 0.33000
+        },
+        delisle: {
+            toKelvin: temp => 373.15 - (temp * 2 / 3),
+            fromKelvin: temp => (temp - 273.15) * 1.5000 - 100.00
+        },
     };
 
     if (sourceUnit != "kelvin") temp = converter[sourceUnit].toKelvin(temp);
